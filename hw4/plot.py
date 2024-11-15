6932346bcfb21be2692c999f101effefb70f9a0d
fegomezl <feg46@pitt.edu>
2024-11-15 15:35:50 -0500

    iaja

--- hw4/plot.py
+++ hw4/plot.py
@@ -5,34 +5,26 @@ import matplotlib
 matplotlib.rcParams['mathtext.fontset'] = 'stix'
 matplotlib.rcParams['font.family'] = 'STIXGeneral'
 
-
 def get_profiling(filename):
-    data = np.genfromtxt(filename)
-    
-    times = []
-    for n in range(1,33):
-        times.append(min(data[:,1][np.where(data[:,0] == 1.0*n)]))
-    speedup = [times[0]/times[n] for n in range(len(times))]
+    files = glob(filename)
+    speedup = np.zeros(32)
+    for file in files:
+        data = np.genfromtxt(file, skip_header=1)
+        times = data[:,1]
+        speedup = [max(speedup[n], times[0]/times[n]) for n in range(len(times))]
     efficiency = [speedup[n]/(n+1) for n in range(len(speedup))]
     return speedup, efficiency
 
-
-files = glob('hw3/results/mmult*')
-for file in files:
-    data = np.genfromtxt(file)
-print(len(data))
-
-'''
 #--------------------------------------------------------------------------------------------
 
 fig, axs = plt.subplots(2, 1)
 
 x = np.linspace(1,32,100)
 axs[0].plot(x, x, color="black")
-axs[1].axhline(y=1, color="black")
+axs[1].axhline(y = 1, color="black")
 
-for N in [1024, 2048, 4096]:
-    speedup, efficiency = get_profiling("results/mmult_{}.txt".format(N))
+for N in [256, 512, 700]:
+    speedup, efficiency = get_profiling("hw4/results/mmult_{}*".format(N))
     
     axs[0].plot(range(1,len(speedup)+1), speedup, marker="o", label="N={}".format(N))
     axs[1].plot(range(1,len(efficiency)+1), efficiency, marker="o", label="N={}".format(N))
@@ -47,7 +39,7 @@ axs[0].legend()
 axs[1].legend()
 
 fig.tight_layout()
-fig.savefig("mmult.eps")
+fig.savefig("hw4/mmult.eps")
 plt.show()
 
 #--------------------------------------------------------------------------------------------
@@ -59,7 +51,7 @@ axs[0].plot(x, x, color="black")
 axs[1].axhline(y=1, color="black")
 
 for N in [6, 7, 8]:
-    speedup, efficiency = get_profiling("results/rect_10_{}.txt".format(N))
+    speedup, efficiency = get_profiling("hw4/results/trape_10_{}*".format(N))
     
     axs[0].plot(range(1,len(speedup)+1), speedup, marker="o", label="N={}".format(10**N))
     axs[1].plot(range(1,len(efficiency)+1), efficiency, marker="o", label="N={}".format(10**N))
@@ -74,7 +66,7 @@ axs[0].legend()
 axs[1].legend()
 
 fig.tight_layout()
-fig.savefig("rect.eps")
+fig.savefig("hw4/trap.eps")
 plt.show()
 
 #--------------------------------------------------------------------------------------------
@@ -85,8 +77,8 @@ x = np.linspace(1,32,100)
 axs[0].plot(x, x, color="black")
 axs[1].axhline(y=1, color="black")
 
-for N in [4096, 8192, 15000]:
-    speedup, efficiency = get_profiling("results/rk_{}.txt".format(N))
+for N in [2048, 4096, 8192]:
+    speedup, efficiency = get_profiling("hw4/results/rk4_{}*".format(N))
     
     axs[0].plot(range(1,len(speedup)+1), speedup, marker="o", label="N={}".format(N))
     axs[1].plot(range(1,len(efficiency)+1), efficiency, marker="o", label="N={}".format(N))
@@ -101,6 +93,5 @@ axs[0].legend()
 axs[1].legend()
 
 fig.tight_layout()
-fig.savefig("rk.eps")
+fig.savefig("hw4/rk4.eps")
 plt.show()
-'''
