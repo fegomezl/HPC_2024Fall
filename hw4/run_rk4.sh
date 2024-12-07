efefd7a2ac164e9162727eca876bb3bce8058206
feg46 <feg46@login3.crc.pitt.edu>
2024-12-06 21:53:10 -0500

    a

--- hw4/run_rk4.sh
+++ hw4/run_rk4.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-for i in $(seq 1 12);
+#SBATCH --job-name=rk4_8192
+#SBATCH --cluster=smp
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-user=feg46@pitt.edu
+#SBATCH --mail-type=FAIL
+#SBATCH --time=2:00:00
+#SBATCH --qos=short
+#SBATCH --output=results/rk4_8192_%A.txt
+
+module load openmpi/4.0.5
+
+make rk4_mpi
+
+for i in $(seq 1 32);
 do
-    echo $i
-    for j in $(seq 1 10);
-    do
-        ./rk3_8_par.x $i
-    done
+	mpirun -n $i ./rk4_mpi.x cluster
 done
--- hw4/run_trape.sh
+++ hw4/run_trape.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-for i in $(seq 1 12);
+#SBATCH --job-name=trape8
+#SBATCH --cluster=smp
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-user=feg46@pitt.edu
+#SBATCH --mail-type=FAIL
+#SBATCH --time=2:00:00
+#SBATCH --qos=short
+#SBATCH --output=results/trape_10_8_%A.txt
+
+module load openmpi/4.0.5
+
+make trape_mpi
+
+for i in $(seq 1 32);
 do
-    echo $i
-    for j in $(seq 1 10);
-    do
-        ./rect_par.x $i
-    done
+	mpirun -n $i ./trape_mpi.x cluster
 done
