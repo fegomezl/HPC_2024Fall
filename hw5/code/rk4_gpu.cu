001b0eccb3047b935764bc86d3e7b49f7e269301
fegomezl <feg46@pitt.edu>
2024-12-08 00:05:47 -0500

    a

--- hw5/code/rk4_gpu.cu
+++ hw5/code/rk4_gpu.cu
@@ -1,121 +1,116 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <sys/time.h>
+#include <sys/time.h> 
+#include <cuda.h>
 
-
-#define PROBLEM_SIZE 8192
-
+#define N 1024
 
 struct timeval startTime;
 struct timeval finishTime;
 double timeIntervalLength;
 
+__global__ void CUDA_rk4_0(const double h, const double pow[], const double c[], const double y[], double k[]){
+	int my_tid = blockDim.x*blockIdx.x + threadIdx.x;
+	if (0 < my_tid && my_tid < N){
+		double my_k = pow[my_tid];
+        for (j = 0; j < N; j++)
+			my_k -= c[my_tid*N+j]*y[j];
+		my_k *= h;
+		k[my_tid] = my_k;
+	}
+}
 
-int main(int argc, char* argv[])
-{
-	int i,j;
-	double h=0.3154;
+__global__ void CUDA_rk4_1(const double h, const double pow[], const double c[], const double y[], const double k_old[], double k_new[]){
+	int my_tid = blockDim.x*blockIdx.x + threadIdx.x;
+	if (0 < my_tid && my_tid < N){
+		double my_k = pow[my_tid];
+        for (j = 0; j < N; j++)
+			my_k -= c[my_tid*N+j]*(y[j]+0.5*k_old[j]);
+		my_k *= h;
+		k_new[my_tid] = my_k;
+	}
+}
+
+__global__ void CUDA_rk4_2(const double h, const double pow[], const double c[], const double y[], 
+						   const double k1[], const double k2[], const double k3[], double k4[], double yout[]){
+	int my_tid = blockDim.x*blockIdx.x + threadIdx.x;
+	if (0 < my_tid && my_tid < N){
+		double my_k = pow[my_tid];
+        for (j = 0; j < N; j++)
+			my_k -= c[my_tid*N+j]*(y[j]+k3[j]);
+		my_k *= h;
+		k4[my_tid] = my_k;
+
+		yout[my_tid] = y[my_tid] + (k1[my_tid] + 2*k2[my_tid] + 2*k3[my_tid] + k4[my_tid])/6.0;
+	}
+}
+
+int main(int argc, char* argv[]){
+
+	// Define variables
+	int i, j;
+	double h;
+	double totalSum;
 	double*  y;
-	double*  yt;
 	double*  k1;
 	double*  k2;
 	double*  k3;
 	double*  k4;
 	double*  pow;
 	double*  yout;
-	double** c;
+	double* c;
 
-	double totalSum=0.0;
+	// Allocate arrays
+	cudaMallocManaged(&y, N∗sizeof(double));
+	cudaMallocManaged(&k1, N∗sizeof(double));
+	cudaMallocManaged(&k2, N∗sizeof(double));
+	cudaMallocManaged(&k3, N∗sizeof(double));
+	cudaMallocManaged(&k4, N∗sizeof(double));
+	cudaMallocManaged(&pow, N∗sizeof(double));
+	cudaMallocManaged(&yout, N∗sizeof(double));
+	cudaMallocManaged(&c, N*N∗sizeof(double));
 
-
-	y    = (double* )malloc(PROBLEM_SIZE*sizeof(double));
-	yt   = (double* )malloc(PROBLEM_SIZE*sizeof(double));
-	k1   = (double* )malloc(PROBLEM_SIZE*sizeof(double));
-	k2   = (double* )malloc(PROBLEM_SIZE*sizeof(double));
-	k3   = (double* )malloc(PROBLEM_SIZE*sizeof(double));
-	k4   = (double* )malloc(PROBLEM_SIZE*sizeof(double));
-	pow  = (double* )malloc(PROBLEM_SIZE*sizeof(double));
-	yout = (double* )malloc(PROBLEM_SIZE*sizeof(double));
-	c    = (double**)malloc(PROBLEM_SIZE*sizeof(double*));
-	for (i=0;i<PROBLEM_SIZE;i++) 
-	{
-		c[i]=(double*)malloc(PROBLEM_SIZE*sizeof(double));
+	// Initialize variables
+	h = 0.3154;
+	totalSum = 0.0;
+	for (i = 0; i < N; i++){
+		y[i] = i*i;
+		pow[i] = 2*i;
+		for (j = 0; j < N; j++)
+			c[i*N+j] = i*i+j;
 	}
-
-	for (i = 0; i < PROBLEM_SIZE; i++) 
-	{
-		y[i]=i*i;
-		pow[i]=i+i;
-		for (j = 0; j < PROBLEM_SIZE; j++)
-		{
-			c[i][j]=i*i+j;
-		}
-	}
-
 	
-	//Get the start time
-	gettimeofday(&startTime, NULL);  	
+	// Get the start time
+	gettimeofday(&startTime, NULL);
 
-	for (i = 0; i < PROBLEM_SIZE; i++) 
-	{ 
-		yt[i] = 0.0;
-		for (j = 0; j < PROBLEM_SIZE; j++) 
-		{
-			yt[i] += c[i][j]*y[j];
-		}
-		k1[i] = h*(pow[i]-yt[i]);
-	}
+	CUDA_rk4_0<<1024,1024>>(h, pow, c, y, k1);
+	cudaDeviceSynchronize();	
 
-	for (i = 0; i < PROBLEM_SIZE; i++) 
-	{
-		yt[i] = 0.0;
-		for (j = 0; j < PROBLEM_SIZE; j++) 
-		{
-			yt[i] += c[i][j]*(y[j]+0.5*k1[j]);
-		}
-		k2[i] = h*(pow[i]-yt[i]);
-	}
+	CUDA_rk4_1<<1024,1024>>(h, pow, c, y, k1, k2);
+	cudaDeviceSynchronize();	
 
-	for (i = 0; i < PROBLEM_SIZE; i++) 
-	{
-		yt[i] = 0.0;
-		for (j = 0; j < PROBLEM_SIZE; j++) 
-		{
-			yt[i] += c[i][j]*(y[j]+0.5*k2[j]);
-		}
-		k3[i] = h*(pow[i]-yt[i]);
-	}
+	CUDA_rk4_1<<1024,1024>>(h, pow, c, y, k2, k3);
+	cudaDeviceSynchronize();	
 
-	for (i =0; i < PROBLEM_SIZE; i++) 
-	{
-		yt[i]=0.0;
-		for (j = 0; j < PROBLEM_SIZE; j++) 
-		{
-			yt[i] += c[i][j]*(y[j]+k3[j]);
-		}
-		k4[i] = h*(pow[i]-yt[i]);
+	CUDA_rk4_2<<1024,1024>>(h, pow, c, y, k1, k2, k3, k4, yout);
+	cudaDeviceSynchronize();
 
-		yout[i] = y[i] + (k1[i] + 2*k2[i] + 2*k3[i] + k4[i])/6.0;
-		totalSum+=yout[i];
-	}
+	// Get the end time
+	gettimeofday(&finishTime, NULL);
 
-	//Get the end time
-	gettimeofday(&finishTime, NULL);  /* after time */
-
+	// Check results
+	for (i = 0; i < N; i++)
+		totalSum += yout[i];
 	printf("\n\ntotalSum=%g\n\n",totalSum);
 	
-	
-	//Calculate the interval length 
+	// Calculate the interval length 
 	timeIntervalLength = (double)(finishTime.tv_sec-startTime.tv_sec) * 1000000 
-	                     + (double)(finishTime.tv_usec-startTime.tv_usec);
-	timeIntervalLength=timeIntervalLength/1000;
+	                   + (double)(finishTime.tv_usec-startTime.tv_usec);
+	timeIntervalLength = timeIntervalLength/1000;
 
-	//Print the interval lenght
+	// Print the interval lenght
 	printf("Interval length: %g msec.\n", timeIntervalLength);
-	
-	
 
-	
 	return 0;
 }
